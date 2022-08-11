c1b8b97315f708c7110f0fd138bdd2f76943a5e0
yzubr <vadzim.vinnikau@gmail.com>
2022-08-11 22:56:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+   
+  
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSmbl = str[i];
+    let indexArr = 0;
+    let indexSubArrElmnt = 0;
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+
+      if (bracketsConfig[j].includes(currentSmbl)) {
+        indexArr = j;
+        indexSubArrElmnt = bracketsConfig[j].indexOf(currentSmbl);
+      };
+    }
+   
+    if (indexSubArrElmnt === 0) {
+      stack.push(currentSmbl)
+    } else {
+      if ((indexSubArrElmnt != 0) && (stack.length != 0)) {
+        let topElements = stack[stack.length - 1];
+        if (bracketsConfig[indexArr][0] === topElements) {
+          stack.pop(currentSmbl);
+        } else return false;
+      } else return false;
+    }  
+  }
+  
+  return stack.length === 0;
 }
